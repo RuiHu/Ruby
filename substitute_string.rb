18c085b07e29692b8084cb54f0610f718ac6c98d
Rui Hu <ruihu1990@hotmail.com>
2014-03-27 16:56:46 -0700

    Update substitute_string.rb

--- substitute_string.rb
+++ substitute_string.rb
@@ -3,6 +3,7 @@ user_input = gets.chomp
 user_input.downcase!
 
 if user_input.include? "s"
+  # gsub stands for global substitution
   user_input.gsub!(/s/, "th")
 else
   puts "Nothing to do here!"
