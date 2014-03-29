70ae7db810fe95a9bd433a08618ab1e02f30be6d
Rui Hu <ruihu1990@hotmail.com>
2014-03-29 16:02:38 -0700

    add block to print using description method

--- print_creator.rb
+++ print_creator.rb
@@ -12,3 +12,7 @@ end
 ruby = Language.new("Ruby", "Yukihiro Matsumoto")
 python = Language.new("Python", "Guido van Rossum")
 javascript = Language.new("JavaScript", "Brendan Eich")
+
+ruby.description
+python.description
+javascript.description
